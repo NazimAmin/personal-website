909f7c83ca4235038750abfe64929e6c312f0fd8
nazim <amin.nazim13@gmail.com>
2015-10-19 01:14:35 -0400

    more add-on

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -2,13 +2,16 @@
 
 $(document).ready(function () {
     particlesJS.load('welcome-header-container', 'scripts/particlesjs-config.json');
-    $('.contact-card').addClass('center-card');
+    $('.fa-heart').hover(function () {
+        $(this).toggleClass('animated rubberBand');
+    });
 });
+/*
 $(window).resize(function () {
     if (document.documentElement.clientWidth <= 650) {
         $('.col-xs-6').addClass('col-md-4').removeClass('col-xs-6');
     } else {
         $('.col-md-4').addClass('col-xs-6');
     }
-}).resize();
+}).resize();*/
 //# sourceMappingURL=main.js.map
